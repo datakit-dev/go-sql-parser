1dcba6b500b4e7e884126786b1a08e9687d4ef5b
Jordan Fowler <me@jordanfowler.co>
2024-04-17 18:54:22 -0600

    Use go:embed for js.

--- internal/init.go
+++ internal/init.go
@@ -1,14 +1,28 @@
 package internal
 
-import "github.com/dop251/goja_nodejs/require"
+import (
+	"embed"
+
+	"github.com/dop251/goja_nodejs/require"
+)
+
+//go:embed js/*
+var js embed.FS
 
 var (
 	registry *require.Registry
 )
 
+func sourceLoader(fs embed.FS) require.SourceLoader {
+	return func(path string) ([]byte, error) {
+		return fs.ReadFile(path)
+	}
+}
+
 func init() {
 	regOpts := []require.Option{
-		require.WithGlobalFolders("../js"),
+		require.WithGlobalFolders("./js"),
+		require.WithLoader(sourceLoader(js)),
 	}
 	registry = require.NewRegistry(regOpts...)
 }
